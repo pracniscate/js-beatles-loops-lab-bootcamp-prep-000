3052928f365d9b52723c7dd0fe85de52ecedf0de
Lorena <lorena.giddens@outlook.com>
2018-04-06 02:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(musicians, instruments) {
 // returns an array of strings containing what instruments each musician plays
+
+function theBeatlesPlay(musicians, instruments) {
   var beatleSentence = [];
 for (var i = 0; i < 1; i++) {
   console.log(musicians[0] + instruments[0]);
