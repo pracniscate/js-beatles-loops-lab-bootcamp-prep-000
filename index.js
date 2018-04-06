f95b3742deb4acb77b98a9cda01a57ad8a136c54
Lorena <lorena.giddens@outlook.com>
2018-04-06 03:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
 // returns an array of strings containing what instruments each musician plays
 
 function theBeatlesPlay(musicians, instruments) {
