f40d2a373cac9c438390d056ed19f672b53228a9
Lorena <lorena.giddens@outlook.com>
2018-04-06 03:21:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ function theBeatlesPlay(musicians, instruments) {
 // returns an array of strings with exclamation points
 
 function johnLennonFacts(array) {
-  const facts = [];
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   
     let i = 0;
     while (i < 4) {
